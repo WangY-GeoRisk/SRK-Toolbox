0c85763d785a1ba54aa32fd13480350051518771
WangY-GeoRisk <yuwang77@gmail.com>
2026-07-09 17:01:00 +0800

    Add files via upload
    Add SRK toolbox files

---
+++ Toolbox/Main_code.py
@@ -0,0 +1,29 @@
+﻿"""
+# -*- coding: utf-8 -*-
+# @Author: cONg
+# @Time 20260324
+"""
+
+import os
+import sys
+import arcpy
+
+_THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+if _THIS_DIR not in sys.path:
+    sys.path.insert(0, _THIS_DIR)
+
+try:
+    import SRK_Tool as SRKT
+except ImportError:
+    raise ImportError(
+        "Failed to import SR_Kriging_Tool. "
+        "Ensure SR_Kriging_Tool.pyc is in the same folder."
+    )
+
+
+def main():
+    SRKT.run()
+
+
+if __name__ == "__main__":
+    main()
